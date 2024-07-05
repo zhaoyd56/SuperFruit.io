deaf77f9471e629f384ff42f1094ce71f9689238
zhaoyd56 <59274405+zhaoyd56@users.noreply.github.com>
2024-07-05 18:30:23 +0800

    test

--- cocos-js/spine-3e0daee9.js
+++ cocos-js/spine-3e0daee9.js
@@ -1,2 +1,1 @@
 System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine-a0005c42.wasm")}}}));
-//# sourceMappingURL=spine-3e0daee9.js.map
--- cocos-js/spine.js-f7f5ea79.js
+++ cocos-js/spine.js-f7f5ea79.js
@@ -1,2 +1,1 @@
 System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine.js.mem-c0c591ab.bin")}}}));
-//# sourceMappingURL=spine.js-f7f5ea79.js.map
